4fc4dba74ee7028bd4bc6b69b34d87e3723de1b6
Conor Murphy <conormurphyis@gmail.com>
2026-08-01 10:56:00 +1000

    use interface to refactor

--- structs_methods_interfaces/structs_test.go
+++ structs_methods_interfaces/structs_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
-	got := Perimeter(rectangle)
+	got := rectangle.Perimeter()
 	want := 40.0
 
 	if got != want {
@@ -13,17 +13,24 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	}
 	t.Run("rectangle", func(t *testing.T) {
 		rectangle := Rectangle{12.0, 6.0}
-		got := Area(rectangle)
 		want := 72.0
 
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+		checkArea(t, rectangle, want)
 	})
 
-	// t.Run("circle", func(t *testing.T) {
-	// 	circle :=
-	// })
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{10}
+		want := 314.1592653589793
+
+		checkArea(t, circle, want)
+	})
 }
