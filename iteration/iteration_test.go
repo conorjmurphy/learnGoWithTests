bcb59e9c4cb37d873de173ef21a67168181e1516
Conor Murphy <conormurphyis@gmail.com>
2026-08-01 10:01:28 +1000

    Refactor to support how many repeats passed in

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -1,10 +1,22 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
+	repeated := Repeat("a", 5)
 	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func TestRepeatNum(t *testing.T) {
+	repeated := Repeat("b", 8)
+	expected := "bbbbbbbb"
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
@@ -13,6 +25,12 @@ func TestRepeat(t *testing.T) {
 
 func BenchmarkRepeat(b *testing.B) {
 	for b.Loop() {
-		Repeat("a")
+		Repeat("a", 5)
 	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 5)
+	fmt.Println(repeated)
+	// Output: aaaaa
 }
