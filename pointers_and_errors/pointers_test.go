f41c5a85f507ffcbc2d613d15592d716f11dac04
Conor Murphy <conormurphyis@gmail.com>
2026-08-01 12:40:53 +1000

    refactor to extract assertion

--- pointers_and_errors/pointers_test.go
+++ pointers_and_errors/pointers_test.go
@@ -3,16 +3,28 @@ package pointersanderrors
 import "testing"
 
 func TestWallet(t *testing.T) {
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 
 		wallet.Deposit(Bitcoin(10))
 
-		got := wallet.Balance()
-		want := Bitcoin(10)
+		assertBalance(t, wallet, Bitcoin(10))
+	})
 
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(60)}
+
+		wallet.Withdraw(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(50))
 	})
 }
