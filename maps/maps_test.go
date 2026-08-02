7bd133bd644ac952870d3386656fac9023ac4ad2
Conor Murphy <conormurphyis@gmail.com>
2026-08-02 14:11:36 +1000

    introduce updating and test

--- maps/maps_test.go
+++ maps/maps_test.go
@@ -42,6 +42,20 @@ func TestAdd(t *testing.T) {
 	})
 }
 
+func TestUpdate(t *testing.T) {
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		newDefinition := "new definition"
+
+		dictionary.Update(word, newDefinition)
+
+		assertDefinition(t, dictionary, word, newDefinition)
+		
+	})
+}
+
 func assertStrings(t testing.TB, got, want string) {
 	t.Helper()
 
